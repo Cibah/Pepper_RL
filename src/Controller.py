b30ee2292bb976d7d883afe5603049ffff77664a
Maik Hinkelmann <m.hinkelmann@hotmail.de>
2019-12-19 18:16:44 +0100

    add socket connection to get delta values

--- src/Controller.py
+++ src/Controller.py
@@ -2,71 +2,83 @@ import argparse
 import warnings
 import time
 import threading
-import reward
-import rewardTrackerThread
 import sys			# used for exit()
 import signal			# to catch Ctrl-C Interrupt
 import readAngles
 import pepperMove
-import ballTracker
-
+import socket
+import thread
 
 ip = "192.168.0.41"
 port = "9559"
 
-# define little signal Handler to shutdown rewardTracker-Thread, when Main-Thread closes
+UDP_IP = "127.0.0.1"
+UDP_PORT = 5005
 
+sock = socket.socket(socket.AF_INET, # Internet
+                     socket.SOCK_DGRAM) # UDP
+sock.bind((UDP_IP, UDP_PORT))
+
+
+delta = ""
+
+def saveData(data):
+    f = open("training_data.txt", "a")
+    f.write(data)
+    f.close()
+
+def readData():
+    f = open("training_data.txt", "r")
+    print(f.read())
+
+def getDelta():
+    global delta
+    while True:
+        data, addr = sock.recvfrom(128) # buffer size is 1024 bytes
+        #print "received message:", data
+        delta= data.decode()
+        #print(delta)
 
 if __name__ == "__main__":
+    global delta
 
-    ap = argparse.ArgumentParser()
-    ap.add_argument("-v", "--video",
-                    help="path to the (optional) video file")
-    ap.add_argument("-b", "--buffer", type=int, default=64,
-                    help="max buffer size")
-    ap.add_argument("-c", "--conf", required=True,
-                    help="path to the JSON configuration file")
-    args = vars(ap.parse_args())
-    event = threading.Event()
-    # Als Thread laufen lassen
-    myBallTrackerThread = ballTracker.BallTracker(args, event)
-    ballTracker.ballRunner()
-
+    t1 = threading.Thread(target=getDelta)
+    t1.start()
+    print("udp thread running...")
     session = pepperMove.init(ip, port)
     pepperMove.roboInit(session)
     winkel = dict()
 
-    myReward = reward.Reward()
-
     winkel = readAngles.readAngles(session)
-    delta = ballTracker.delta
+    delta1 = delta
     # neues Delta 1x ausgeben
     params = dict()
     # params["LShoulderPitch"] = [0.0972665, 0.96]
-    params["RShoulderPitch"] = [0.0972665, 0.96]
+    params["RShoulderPitch"] = [0.1272665, 0.3]
 
     # params["LHand"] = [0.88, 0.96]
     # params["RHand"] = [0.88, 0.96]
 
     # params["LWristYaw"] = [-1.309, 0.96]
-    params["RWristYaw"] = [1.409, 0.96]
+    params["RWristYaw"] = [2.409, 0.3]
 
     # params["LShoulderRoll"] = [0.10472, 0.96]
-    params["RShoulderRoll"] = [-0.10472, 0.96]
+    params["RShoulderRoll"] = [0.30472, 0.3]
     service = session.service("ALMotion")
     pepperMove.move(params, service)
 
-    delta2 = ballTracker.delta
+    delta2 = delta
     winkel2 = readAngles.readAngles(session)
-    rewards = int((int(delta) - int(delta2)))
+    print("rewards= "+ delta1+" - "+ delta2)
+    #rewards = int((int(delta) - int(delta2)))
+    rewards = delta
     print("####### Run ######")
     print("Delta:\t" + str(delta))
     print("Winkel:\t" + str(winkel))
     print("Delta2:\t" + str(delta2))
     print("Winkel2:\t" + str(winkel2))
     print("rewards:\t" + str(rewards))
-    # import this class # Rewards.updateRewards(delta)
-    shutdown(signal.SIGINT, None)
+    exportData= delta +"|"+ winkel +"\n"+ delta2 +"|"+ winkel2 +"\n" + rewards
+    saveData(exportData)
 
-
-def funktion(session):
+exit()
--- src/ballTracker.py
+++ src/ballTracker.py
@@ -1,5 +1,6 @@
+#-*- coding:utf-8 -*-
 # testBallTracker.py
-# kleine Test-Applikation, um die reward-Funktionalität des BallTrackers zu testen
+# kleine Test-Applikation, um die reward-Funktionalitaet des BallTrackers zu testen
 # nutzt aber jetzt RewardTrackerThread-Klasse, d.h. abgeleitete Thread-Klasse
 #
 import argparse
@@ -10,48 +11,55 @@ import reward
 import rewardTrackerThread
 import sys			# used for exit()
 import signal			# to catch Ctrl-C Interrupt
-import readAngles
-import pepperMove
+import socket
 
-
+UDP_IP = "127.0.0.1"
+UDP_PORT = 5005
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
+    
 # define little signal Handler to shutdown rewardTracker-Thread, when Main-Thread closes
+def shutdown(sig, frame):
+    print ("Closing...")
+    event.set()
+    # trackingThread.join()	# not requires because of event
+    sys.exit(0)
 
+def updateDelta(d1):
+    MESSAGE = str(d1)
+    sock.sendto(MESSAGE.encode(), (UDP_IP, UDP_PORT)) 
 
-class BallTracker (threading.Thread):
+if __name__ == "__main__":
+    myReward = reward.Reward()
+    # myReward.print()
+    signal.signal(signal.SIGINT, shutdown)
 
-    def __init__(self, arguments, event):
-        threading.Thread.__init__(self)
-        self.args = arguments
-        self.delta = (0.0, 0.0,)
+    # construct the argument parse and parse the arguments
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-v", "--video",
+        help="path to the (optional) video file")
+    ap.add_argument("-b", "--buffer", type=int, default=64,
+        help="max buffer size")
+    ap.add_argument("-c", "--conf", required=True,
+        help="path to the JSON configuration file")
+    args = vars(ap.parse_args())
+    # filter warnings
+    warnings.filterwarnings("ignore")
+    
+    event = threading.Event()	# used to stop trackingThread 
+    myRewardTrackerThread = rewardTrackerThread.RewardTrackerThread( myReward, args, event)
+    # nicht mehr nötig
+    # trackingThread = threading.Thread(target= myRewardTracker.run ) # , daemon= True)
+    # trackingThread.start()
+    myRewardTrackerThread.start() 
 
-   def shutdown(sig, frame):
-        print ("Closing...")
-        event.set()
-        # trackingThread.join()	# not requires because of event
-        sys.exit(0)
+    while True :
+        delta= myReward.getDeltaIfNew();
+        # neues Delta 1x ausgeben
+        if delta is not None :
+           print ("Reward= ", delta)
+           updateDelta(delta)       
+           #import this class # Rewards.updateRewards(delta)
+        time.sleep(0.01)
 
-    def ballRunner():
-        myReward = reward.Reward()
-        # myReward.print()
-        signal.signal(signal.SIGINT, shutdown)
-        # filter warnings
-        warnings.filterwarnings("ignore")
-
-        event = threading.Event()  # used to stop trackingThread
-        myRewardTrackerThread = rewardTrackerThread.RewardTrackerThread(
-            myReward, args, event)
-        # nicht mehr nötig
-        # trackingThread = threading.Thread(target= myRewardTracker.run ) # , daemon= True)
-        # trackingThread.start()
-        myRewardTrackerThread.start()
-
-        while True:
-            self.delta = myReward.getDeltaIfNew()
-            # neues Delta 1x ausgeben
-            if self.delta is not None:
-                print ("Delta= ", self.delta)
-                # import this class # Rewards.updateRewards(delta)
-            time.sleep(0.01)
-
-        shutdown(signal.SIGINT, None)
+    shutdown(signal.SIGINT, None)
 # Ende
