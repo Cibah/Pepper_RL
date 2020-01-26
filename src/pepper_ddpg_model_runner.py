f6a107d581ed521af3a1cf4f1bca90a9d15c4cbd
Artur Dick <artur.dick@mailbox.org>
2020-01-26 13:14:58 +0100

    Prepare running multiple models for evaluation
    Signed-off-by: Artur Dick <artur.dick@mailbox.org>

--- src/pepper_ddpg_model_runner.py
+++ src/pepper_ddpg_model_runner.py
@@ -30,47 +30,49 @@ def main():
                                actor.get_num_trainable_vars())
 
         actor_noise = OrnsteinUhlenbeckActionNoise(mu=np.zeros(action_dim))
-
         saver = tf.train.Saver()
+
+        # TODO Loop over all generated models
         saver.restore(sess, args['model'])
-        runModel(session, thread1, actor, critic, actor_noise)
+        print(runModel(session, thread1, actor, critic, actor_noise))
 
 
 def runModel(session, thread, actor, critic, actor_noise):
-    # run the model forever
-    while True:
-        ep_reward = 0
-        for j in range(int(args['max_episode_len'])):
-            service = session.service("ALMotion")
-            params = dict()
-            # Hole Anfangszustand
-            delta1 = thread.delta[0]
-            winkel1 = readAngle(session)
-            s = [winkel1, delta1]
-            a = actor.predict(np.reshape(s, (1, actor.s_dim))) + actor_noise()
+    highestReward = 0
+    lowestReward = 0
+    ep_reward = 0
+    for j in range(int(args['max_episode_len'])):  # make this to 100 steps
+        service = session.service("ALMotion")
+        params = dict()
+        # Hole Anfangszustand
+        delta1 = thread.delta[0]
+        winkel1 = readAngle(session)
+        s = [winkel1, delta1]
+        a = actor.predict(np.reshape(s, (1, actor.s_dim))) + actor_noise()
+        threshold_reward = 0
+        if a[0] < UNTERE_GRENZE:
+            a[0] = UNTERE_GRENZE
+            threshold_reward = -1000
 
-            threshold_reward = 0
-            if a[0] < UNTERE_GRENZE:
-                a[0] = UNTERE_GRENZE
-                threshold_reward = -1000
+        if a[0] > OBERE_GRENZE:
+            a[0] = OBERE_GRENZE
+            threshold_reward = -1000
 
-            if a[0] > OBERE_GRENZE:
-                a[0] = OBERE_GRENZE
-                threshold_reward = -1000
+        # Fuehre Action aus
+        params[args['motor']] = [a[0], TIME_TO_MOVE]
+        Pepper.move(params, service)
 
-            # Fuehre Action aus
-            params[args['motor']] = [a[0], TIME_TO_MOVE]
-            Pepper.move(params, service)
+        # Hole Folgezustand
+        delta2 = thread.delta[0]
 
-            # Hole Folgezustand
-            delta2 = thread.delta[0]
-
-            # Hole Reward
-            r = getReward(delta2) + threshold_reward
-            ep_reward += r
-
-    print("Episode: " + str(i) + "\t" + str(ep_reward / int(args['max_episode_len'])))
-
+        # Hole Reward
+        r = getReward(delta2, a[0]) + threshold_reward
+        if (r > highestReward):
+            highestReward = r
+        if (r < lowestReward):
+            lowestReward = r
+        ep_reward += r
+    return (str(lowestReward) + "\t" + str(highestReward) + "\t" + str(ep_reward / int(args['max_episode_len'])))
 
 if __name__ == '__main__':
     main()
