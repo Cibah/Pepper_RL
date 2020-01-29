3c1241017575617e66988e0743a0fa31d0d53478
Artur Dick <artur.dick@mailbox.org>
2020-01-29 13:24:12 +0100

    84% Accuracy with ELU Activation
    Signed-off-by: Artur Dick <artur.dick@mailbox.org>

--- src/pepper_ddpg_model_evaluator.py
+++ src/pepper_ddpg_model_evaluator.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+# Run with newer tensorflow version.
 from keras import Sequential
 from keras.layers import Dense
 import numpy
@@ -14,9 +15,9 @@ Y = dataset[:, 4:6]
 
 # create model
 model = Sequential()
-model.add(Dense(3, input_dim=3, kernel_initializer='uniform', activation='relu'))
-model.add(Dense(2, kernel_initializer='uniform', activation='relu'))
-model.add(Dense(2, kernel_initializer='uniform', activation='relu'))
+model.add(Dense(3, input_dim=3, kernel_initializer='uniform', activation='elu'))
+model.add(Dense(2, kernel_initializer='uniform', activation='elu'))
+model.add(Dense(2, kernel_initializer='uniform', activation='elu'))
 
 # Compile model
 model.compile(loss='mean_absolute_error', optimizer='SGD', metrics=['accuracy'])
@@ -24,10 +25,15 @@ model.compile(loss='mean_absolute_error', optimizer='SGD', metrics=['accuracy'])
 # Fit the model
 # Epoch Anzahl, Batch Size
 print("Training Model")
-model.fit(X, Y, epochs=150, batch_size=64, verbose=1, shuffle=1, validation_split=0.8)
+model.fit(X, Y, epochs=150, batch_size=256, verbose=1, shuffle=1, validation_split=0.7)
 
 print("Evaluate")
 # evaluate the model
 scores = model.evaluate(X, Y)
-# scores = model.evaluate(INPUT1, INPUT2, INPUT3, OUTPUT1, OUTPUT2)
 print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1] * 100))
+
+print("Predict")
+pred = numpy.array([[-0.0153398513794, 237, 0.4]], numpy.float)
+print(model.predict(pred, batch_size=None, verbose=0, steps=None, callbacks=None, max_queue_size=10, workers=1,
+                    use_multiprocessing=False))
+print("Should be: 224,-10124")
